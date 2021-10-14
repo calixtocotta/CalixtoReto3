4203ad3812c853705129572183185a14d2f253cf
calixto cotta <ccottav14@gmail.com>
2021-10-14 12:16:40 -0500

    impor de GenerationType

--- src/main/java/reto3/reto3/Entidad/Message.java
+++ src/main/java/reto3/reto3/Entidad/Message.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
